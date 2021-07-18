9f0f761a3960e6031a589b668badd57b05ba3a3c
Rosen Kanev <rosen.kanev@mentormate.com>
2021-07-18 22:46:57 +0300

    refactor: remove unused vite build config

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,5 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
-import path from 'path';
 
 export default {
     plugins: [reactRefresh()],
-    build: {
-        lib: {
-            entry: path.resolve(__dirname, 'src/Tree.jsx'),
-            name: 'ReactTree',
-        },
-        rollupOptions: {
-            external: ['react'],
-            output: {
-                globals: {
-                    react: 'React',
-                },
-            },
-        },
-    },
 };
