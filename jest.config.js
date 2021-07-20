e878818f320e4bd859e920c8ae9bb431d15085ee
Rosen Kanev <rosen.kanev@mentormate.com>
2021-07-20 23:06:02 +0300

    tests: ignore code coverage for React Context

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     roots: ['<rootDir>/src'],
-    collectCoverageFrom: ['src/Tree.jsx', 'src/TreeItem.jsx', 'src/TreeContext.js', 'src/useInternalState.js'],
+    collectCoverageFrom: ['src/Tree.jsx', 'src/TreeItem.jsx', 'src/useInternalState.js'],
     setupFilesAfterEnv: ['@testing-library/jest-dom', '@testing-library/jest-dom/extend-expect'],
     testMatch: ['<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}', '<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}'],
     testEnvironment: 'jsdom',
